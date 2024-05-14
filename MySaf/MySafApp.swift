93f1bf3b10b53196e3b16e1744463e8056f0ffab
Huss3n <aisackhussein@gmail.com>
2024-05-14 16:49:21 +0300

    first commit

---
+++ MySaf/MySafApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySafApp.swift
+//  MySaf
+//
+//  Created by Muktar Hussein on 14/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MySafApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
